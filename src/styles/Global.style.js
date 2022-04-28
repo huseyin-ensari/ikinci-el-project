8685ea5d409645eed3d9d703b73ae94530452514
Hüseyin Ensari <hsynensari@gmail.com>
2022-04-28 18:34:35 +0300

    Add global style and theme configurations

---
+++ src/styles/Global.style.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body{
+        background-color: #F2F2F2;
+        font-family: 'Nunito', sans-serif;
+        font-size: 10px;
+        /* min-height: 100vh; */
+    }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/Theme.style.js
@@ -0,0 +1,28 @@
+const theme = {
+    colors: {
+        blue: '#4B9CE2',
+        lightBlue: '#F0F8FF',
+        red: '#F77474',
+        lightRed: '#FFF2F2',
+        white: '#FFFFFF',
+        lightBlack: '#525252',
+        black: '#3E3E3E',
+        grey: '#B1B1B1',
+        bgGrey: '#F2F2F2',
+        inputGrey: '#F4F4F4',
+        orange: '#FAAD60',
+        lightOrange: '#FFF0E2',
+        green: '#46AF32',
+        lightGreen: '#F1FFF0'
+    },
+    responsive: {
+        mobile: '475px',
+        tablet: '768px',
+        laptop: '1200px',
+        desktop: '1500px',
+        largeDesktop: '1800px',
+        xLargeDesktop: '2000px'
+    }
+};
+
+export default theme;
