6d2b1ca8958d6dfd030855de731dd04a05b779b0
Hüseyin Ensari <hsynensari@gmail.com>
2022-04-28 18:35:39 +0300

    Add axios configuration

---
+++ src/services/axiosConfigs.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+
+export const api = axios.create({ baseURL });
+api.interceptors.request.use(
+    async (config) => {
+        const accessToken = await localStorage.getItem('access-token');
+        if (accessToken) {
+            config.headers.authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export const URL = {
+    auth: `${baseURL}/auth`,
+    products: '/products',
+    users: '/users'
+};
