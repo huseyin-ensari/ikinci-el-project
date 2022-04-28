bc2e99fb3f80bb4b8ff4b82402a5f1eb27e446a1
Hüseyin Ensari <hsynensari@gmail.com>
2022-04-28 18:32:21 +0300

    Create basic text components

---
+++ src/components/basics/Texts.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Text = styled.span`
+    font-size: 15px;
+    height: 20px;
+    color: ${({ theme }) => theme.colors.lightBlack};
+`;
+
+export const SmallText = styled.span`
+    font-size: 12px;
+    color: ${({ theme }) => theme.colors.grey};
+`;
+
+export const Title = styled.span`
+    font-size: 32px;
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.lightBlack};
+`;
+
+export const TextLink = styled.a`
+    color: ${({ theme }) => theme.colors.blue};
+    font-size: 15px;
+    font-weight: bold;
+    text-decoration: none;
+    &:hover {
+        cursor: pointer;
+        text-decoration: underline;
+    }
+`;
