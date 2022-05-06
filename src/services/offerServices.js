435b9ca398a697a88133ed8a6ad2c35807ec46c8
Hüseyin Ensari <hsynensari@gmail.com>
2022-05-06 04:51:40 +0300

    Add offer service

---
+++ src/services/offerServices.js
@@ -0,0 +1,28 @@
+import { URL, api } from './axiosConfigs';
+
+export const fetchGiveOffer = async (productID, userID, offerPrice) => {
+    const result = await api.post(`${URL.offers}`, {
+        product: productID,
+        users_permissions_user: userID,
+        offerPrice: Number(offerPrice),
+        isStatus: true
+    });
+    return result;
+};
+
+export const fetchDeleteOffer = async (offerID) => {
+    const result = await api.delete(`${URL.offers}/${offerID}`);
+    return result;
+};
+
+export const fetchMyBids = async (userID) => {
+    const result = await api.get(
+        `${URL.offers}?users_permissions_user=${userID}`
+    );
+    return result;
+};
+
+export const fetchOfferByID = async (offerID) => {
+    const result = await api.get(`${URL.offers}/${offerID}`);
+    return result;
+};
