34613e097bf3052c3782605f3d84d4bc6dc42c47
Hüseyin Ensari <hsynensari@gmail.com>
2022-05-09 08:28:55 +0300

    Add infinite scroll

--- src/contexts/productsContext.js
+++ src/contexts/productsContext.js
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect, useContext } from 'react';
 import { Loader } from '../components/basics';
 import { fetchCategories } from '../services/categoryServices';
-import { fetchProducts } from '../services/productServices';
+import { fetchProductCount, fetchProducts } from '../services/productServices';
 
 const ProductsContext = createContext();
 
@@ -9,29 +9,63 @@ const ProductsContextProvider = ({ children }) => {
     const [allProducts, setAllProducts] = useState([]);
     const [categories, setCategories] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [productCount, setProductCount] = useState(0);
+    const [page, setPage] = useState(1);
 
     const getCategories = async () => {
-        const response = await fetchCategories();
-        setCategories([...response.data]);
+        const { data } = await fetchCategories();
+        setCategories([...data]);
+    };
+
+    const getProductCount = async () => {
+        const { data } = await fetchProductCount();
+        setProductCount(data);
+    };
+
+    const nextPage = () => setPage(page + 1);
+    const resetPage = (newProduct) => {
+        setAllProducts([newProduct, ...allProducts]);
     };
 
     const getProducts = async () => {
-        const response = await fetchProducts();
-        setAllProducts([...response.data]);
+        const limit = 15;
+        let skipProducts = productCount - limit * page;
+        if (skipProducts <= 0) skipProducts = 0;
+        const { data } = await fetchProducts(skipProducts);
+        return data.reverse();
     };
 
     useEffect(() => {
         const setData = async () => {
-            await getCategories();
-            await getProducts();
+            const response = await getProducts();
+            setAllProducts([...allProducts, ...response]);
+        };
+        if (productCount !== 0) setData();
+    }, [page]);
+
+    useEffect(() => {
+        const setData = async () => {
+            await getProductCount();
+            const response = await getProducts();
+            setAllProducts([...response]);
         };
         setData();
         setLoading(false);
+    }, [productCount]);
+
+    useEffect(() => {
+        const setData = async () => {
+            await getCategories();
+        };
+        setData();
     }, []);
 
     const values = {
         allProducts,
-        categories
+        categories,
+        nextPage,
+        resetPage,
+        productCount
     };
 
     if (loading) {
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Link, useParams } from 'react-router-dom';
 import { CategoryPanel } from '../../components';
-import { BoldText, Container, Text } from '../../components/basics';
+import { BoldText, Container, Loader, Text } from '../../components/basics';
 import { banner } from '../../constants/images';
 import { useProducts } from '../../contexts/productsContext';
 import BasicLayout from '../../layouts/Basic';
@@ -15,15 +16,18 @@ import {
     Price
 } from './style';
 
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+
 const HomePage = () => {
     const [products, setProducts] = useState([]);
     const [activeCategory, setActiveCategory] = useState('all');
-    const { allProducts } = useProducts();
-    const baseURL = process.env.REACT_APP_API_BASE_URL;
-
+    const { allProducts, nextPage, productCount } = useProducts();
+    const { categoryID } = useParams();
     useEffect(() => {
-        setProducts([...allProducts]);
-    }, [allProducts]);
+        categoryID
+            ? setActiveCategory(Number(categoryID))
+            : setActiveCategory('all');
+    }, []);
 
     useEffect(() => {
         if (activeCategory === 'all') {
@@ -31,10 +35,10 @@ const HomePage = () => {
             return;
         }
         const filteredList = allProducts.filter(
-            (product) => product.category.id === activeCategory
+            (product) => product?.category?.id === activeCategory
         );
         setProducts([...filteredList]);
-    }, [activeCategory]);
+    }, [activeCategory, allProducts]);
 
     return (
         <BasicLayout>
@@ -47,36 +51,48 @@ const HomePage = () => {
                 {products.length === 0 && (
                     <Empty>Bu kategoride hiç ürün yok.</Empty>
                 )}
-                <ProductList>
-                    {products.length > 0 &&
-                        products.reverse().map((product) => (
-                            <Link
-                                to={`/product/${product?.id}`}
-                                key={product?.id}>
-                                <ProductCard>
-                                    <div>
-                                        <img
-                                            src={baseURL + product?.image?.url}
-                                            onError={(e) => {
-                                                e.currentTarget.src =
-                                                    imgNotFound;
-                                            }}
-                                        />
-                                    </div>
-                                    <ProductInformation>
-                                        <BoldText size={15}>
-                                            {product?.brand}
-                                        </BoldText>
-                                        <Text size={13}>
-                                            <b>Renk: </b>
-                                            {product?.color}
-                                        </Text>
-                                    </ProductInformation>
-                                    <Price>{product?.price} TL</Price>
-                                </ProductCard>
-                            </Link>
-                        ))}
-                </ProductList>
+                <InfiniteScroll
+                    dataLength={allProducts.length}
+                    next={nextPage}
+                    endMessage={<h1>Bütün Ürünleri Gördünüz</h1>}
+                    hasMore={productCount > allProducts.length}
+                    style={{ overflow: 'hidden' }}
+                    initialScrollY={300}
+                    loader={<Loader />}>
+                    <ProductList>
+                        {products.length > 0 &&
+                            products.map((product) => (
+                                <Link
+                                    to={`/product/${product?.id}`}
+                                    key={product?.id}>
+                                    <ProductCard>
+                                        <div>
+                                            <img
+                                                src={
+                                                    baseURL +
+                                                    product?.image?.url
+                                                }
+                                                onError={(e) => {
+                                                    e.currentTarget.src =
+                                                        imgNotFound;
+                                                }}
+                                            />
+                                        </div>
+                                        <ProductInformation>
+                                            <BoldText size={15}>
+                                                {product?.brand}
+                                            </BoldText>
+                                            <Text size={13}>
+                                                <b>Renk: </b>
+                                                {product?.color}
+                                            </Text>
+                                        </ProductInformation>
+                                        <Price>{product?.price} TL</Price>
+                                    </ProductCard>
+                                </Link>
+                            ))}
+                    </ProductList>
+                </InfiniteScroll>
             </Container>
         </BasicLayout>
     );
